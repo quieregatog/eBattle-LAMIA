ed501cd451ea55dc88847a5799779a3491f6cbcb
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2023-03-21 15:14:23 -0300

    Change wase component

--- src/components/Details/styled.ts
+++ src/components/Details/styled.ts
@@ -3,11 +3,10 @@ import styled from "styled-components";
 export const StyledWave = styled.div`
   position: absolute;
   bottom: 0;
-  left: 0;
-  right: 0;
-  padding: 0;
-
   img {
     width: 100%;
+    padding: 0;
+    margin: 0;
+    margin-bottom: -12px;
   }
 `;
--- src/pages/styled.ts
+++ src/pages/styled.ts
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const PageStyled = styled.div`
   width: 100%;
   display: block;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 `;
